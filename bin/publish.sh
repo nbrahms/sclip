4a21d7133c390f1807ed990060bc4a62f164899e
nathan <nathan@meraki.com>
2015-04-05 00:41:04 -0700

    Add to git on publish

--- bin/publish.sh
+++ bin/publish.sh
@@ -9,7 +9,8 @@ sed -i .bak -e 's/version := "\(.*\)-SNAPSHOT"/version := "\1"/' build.sbt
 for V in $VERSIONS
 do
     sed -i .bak -e 's/scalaVersion := .*/scalaVersion := "'$V'"/' build.sbt
-    sbt test publish
+    sbt clean test publish
+    git add releases
 done
 
 git co build.sbt
